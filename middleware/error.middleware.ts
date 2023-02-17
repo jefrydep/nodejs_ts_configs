3b9767cc23047df2f39ab246c5ad493cf2f8f716
jefry <jefrydep@gmail.com>
2023-02-17 11:38:23 -0500

    tag ready

---
+++ middleware/error.middleware.ts
@@ -0,0 +1,14 @@
+import { Prisma } from "@prisma/client";
+import { Response, Request, NextFunction } from "express";
+import { errorProp } from "../utils/format.server";
+
+export const handleError = async (
+  error: errorProp,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { status, message, errorContent } = error;
+  console.log(error);
+  res.status(status).json({ message, errorContent });
+};
