ae0d7461beae4803898878e9642f54e0cfb37a22
Jefry <jefrydep@gmail.com>
2023-02-21 08:21:00 -0500

    validating fields

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,18 @@
 import express, { Router } from "express";
+import { check } from "express-validator/src/middlewares/validation-chain-builders";
 import { Register, login } from "../controllers/auth.controller";
+import { validateFields } from "../middleware/validateFields";
 // import { login } from "../controllers/auth.controller";
  
 const app = express();
 const autRouter = Router();
-autRouter.post("/auth", login);
-autRouter.post("/auth/register",Register);
+autRouter.post("/auth",[
+    check('email','El email es obligatorio').isEmail(),
+    check('password','el password debe tener minimo 5 caracteres').isLength({min:5}),
+    validateFields
+], login);
+autRouter.post("/auth/register",[
+    check('firstName','tha name is required'),
+    validateFields],Register);
 
 export {autRouter}
